35f0c749ab2479c0e7da6e460759ba501f240221
phetzy <david.j.fetzer@gmail.com>
2025-01-15 22:24:21 -0500

    fixed search redirect

--- app/shop/search/layout.tsx
+++ app/shop/search/layout.tsx
@@ -2,13 +2,13 @@ import Footer from 'components/layout/footer';
 import Collections from 'components/layout/search/collections';
 import FilterList from 'components/layout/search/filter';
 import { sorting } from 'lib/constants';
-import ChildrenWrapper from './children-wrapper';
 import { Suspense } from 'react';
+import ChildrenWrapper from './children-wrapper';
 
 export default function SearchLayout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense>
-      <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 px-4 pb-4 text-foreground dark:text-foreground md:flex-row">
+      <div className="mx-auto flex max-w-screen-2xl flex-col gap-8 pt-8 px-4 pb-4 text-foreground dark:text-foreground md:flex-row">
         <div className="order-first w-full flex-none md:max-w-[125px]">
           <Collections />
         </div>
