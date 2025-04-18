c1098241784ab7ba3bc3ccb6d4beba765586ae42
phetzy <david.j.fetzer@gmail.com>
2025-04-18 00:41:55 -0600

    logo

--- app/cart/thank-you/page.tsx
+++ app/cart/thank-you/page.tsx
@@ -1,9 +1,8 @@
 "use client";
-import { AspectRatio } from "@/components/ui/aspect-ratio";
 import Image from 'next/image';
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import Logo from '../../public/images/logo-square.png';
+import Logo from '../public/images/logo.png';
 
 export default function CartThankYou() {
   const router = useRouter();
@@ -16,9 +15,19 @@ export default function CartThankYou() {
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-24">
-      <AspectRatio className="w-16 h-16 mb-4">
-        <Image src={Logo} alt="Logo" width={100} height={100} />
-      </AspectRatio>
+      <div className="flex justify-center">
+        <Image
+          src={Logo}
+          alt="GripKeeper Logo"
+          width={600}
+          height={600}
+                className="object-center"
+                priority
+                quality={100}
+                loading="eager"
+                fetchPriority="high"
+              />
+            </div>
       <h1 className="text-3xl font-bold mb-4">Thank you for your order!</h1>
       <p className="mb-2">Your cart has been cleared.</p>
       <p className="text-neutral-500 mb-8">You can close this page or continue shopping.</p>
